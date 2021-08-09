de57b8e6165c3f092c33b5bd6455a131f9a7b67e
sjswjeon <sangwon.jeon.uk@gmail.com>
2021-08-09 23:17:57 +0900

    Pagination
    todo:
    Search bar

--- boardpractice/src/main/java/com/example/boardpractice/controller/BoardController.java
+++ boardpractice/src/main/java/com/example/boardpractice/controller/BoardController.java
@@ -4,13 +4,18 @@ import com.example.boardpractice.model.Board;
 import com.example.boardpractice.repository.BoardRepository;
 import com.example.boardpractice.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @Controller
 @RequestMapping("/board")
@@ -23,9 +28,14 @@ public class BoardController {
     private BoardValidator boardValidator;
 
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Board> boards = repository.findAll();
+    public String list(Model model, @PageableDefault(size = 2) Pageable pageable) {
+        Page<Board> boards = repository.findAll(pageable);
+        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
         model.addAttribute("boards", boards);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
         return "board/list";
     }
 
