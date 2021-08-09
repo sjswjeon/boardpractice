5b5f0c4f17d7ceaa551166254e2599a61599ed69
sjswjeon <sangwon.jeon.uk@gmail.com>
2021-08-09 21:47:04 +0900

    init

---
+++ boardpractice/src/main/java/com/example/boardpractice/BoardpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.boardpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardpracticeApplication.class, args);
+	}
+
+}
---
+++ boardpractice/src/test/java/com/example/boardpractice/BoardpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boardpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
