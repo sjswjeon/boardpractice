d5385a441631b980c8b2cbf2e6714359f5ca54ab
sjswjeon <sangwon.jeon.uk@gmail.com>
2021-08-10 04:27:40 +0900

    securing practice 2222

---
+++ boardpractice/src/main/java/com/example/boardpractice/service/UserService.java
@@ -0,0 +1,30 @@
+package com.example.boardpractice.service;
+
+import com.example.boardpractice.model.Role;
+import com.example.boardpractice.model.User;
+import com.example.boardpractice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User save(User user) {
+
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        user.setEnabled(true);
+        Role role = new Role();
+        role.setId(2L);
+        user.getRoles().add(role);
+
+        return userRepository.save(user);
+    }
+}
