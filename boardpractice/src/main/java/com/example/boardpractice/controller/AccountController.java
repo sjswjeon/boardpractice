fd0ad87970a8a06470f406a8beaa378cf06176ee
sjswjeon <sangwon.jeon.uk@gmail.com>
2021-08-10 05:11:36 +0900

    securing practice 33333

---
+++ boardpractice/src/main/java/com/example/boardpractice/controller/AccountController.java
@@ -0,0 +1,38 @@
+package com.example.boardpractice.controller;
+
+import com.example.boardpractice.model.User;
+import com.example.boardpractice.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/account")
+public class AccountController {
+
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping("/signin")
+    public String signIn() {
+        return "account/signin";
+    }
+
+    @PostMapping("/signin")
+    public String login() {
+        return "redirect:/";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "account/register";
+    }
+
+    @PostMapping("/register")
+    public String signUp(User user) {
+        boardService.save(user);
+        return "redirect:/";
+    }
+}
